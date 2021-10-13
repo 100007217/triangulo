f0c07eab58c557b21531b5c43dffb97b1568715c
100007217 <gomezmonterroso14@gmail.com>
2021-10-13 17:36:31 +0200

    Numero negativos no aceptados

--- js/code.js
+++ js/code.js
@@ -1,16 +1,20 @@
 var textofila = "";
 var textocompleto = ""
 var filas = prompt("Cuantas filas")
-for (let columna = 0; columna < 1; columna++) {
+if (filas < 0) {
+    alert("No metas un numero negativo porfa")
+} else {
+    for (let columna = 0; columna < 1; columna++) {
 
-    //console.log('Recursion por columna' + columna)
-    for (let fila = 0; fila < filas; fila++) {
-        //console.log('Recursion por fila' + fila)
-        textofila = textofila + "#"
-        console.log(textofila)
-        textocompleto = textocompleto + "<br>"
-        textocompleto = textocompleto + textofila
+        //console.log('Recursion por columna' + columna)
+        for (let fila = 0; fila < filas; fila++) {
+            //console.log('Recursion por fila' + fila)
+            textofila = textofila + "#"
+            console.log(textofila)
+            textocompleto = textocompleto + "<br>"
+            textocompleto = textocompleto + textofila
+        }
+        textofila = ""
     }
-    textofila = ""
+    document.getElementById("viva").innerHTML = textocompleto;
 }
-document.getElementById("viva").innerHTML = textocompleto;
